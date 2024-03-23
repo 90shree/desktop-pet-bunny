7bfaa2572a05d792764a185cf9cc49a7014e497d
shree <163702108+90shree@users.noreply.github.com>
2024-03-23 18:53:34 -0400

    Update virtualpet.py

--- desktop-pet/code/virtualpet.py
+++ desktop-pet/code/virtualpet.py
@@ -30,24 +30,24 @@ class Bunny:
 
     def load_gifs(self):
         self.sleep = [tk.PhotoImage(file='sleep.gif', format='gif -index %i' % i) for i in range(7)]
-        self.eat = [tk.PhotoImage(file='eat.gif', format='gif -index %i' % i) for i in range(13)]
+        self.eat = [tk.PhotoImage(file='eat.gif', format='gif -index %i' % i) for i in range(14)]
         self.walk_left = [tk.PhotoImage(file='walkleft.gif', format='gif -index %i' % i) for i in range(6)]
         self.walk_right = [tk.PhotoImage(file='walkright.gif', format='gif -index %i' % i) for i in range(6)]
         self.idle = [tk.PhotoImage(file='idle.gif', format='gif -index %i' % i) for i in range(2)]
+        self.idle_to_sleep = [tk.PhotoImage(file='idle_to_sleep.gif', format='gif -index %i' % i) for i in range(7)]
 
     def init_variables(self):
         self.current_action = None
-        self.img = self.sleep[0]
+        self.img = self.idle[0]
         self.y = self.window.winfo_screenheight() - 110
         self.x = (self.window.winfo_screenwidth() - 110) / 2
         self.vel_x = 0
         self.vel_y = 0
-        self.gravity = 9.81
-        self.drag_acceleration = 0.5
-        self.drag_deceleration = 9
+        self.gravity = 0.981
         self.last_action_time = time.time()
         self.last_change_time = time.time()
         self.action_duration = 0
+        self.moving_back = False
 
     def update(self):
         self.update_movement()
@@ -57,13 +57,13 @@ class Bunny:
     def update_movement(self):
         current_time = time.time()
 
-        if current_time > self.last_change_time + 7:
+        if current_time > self.last_change_time + 7 and not self.moving_back:
             self.last_change_time = current_time
             self.img = self.idle[0]
-            self.action_duration = 10
+            self.action_duration = 7
             self.last_action_time = current_time
 
-            if self.current_action is None:
+            if self.current_action is None or self.current_action == 'idle':
                 random_action = random.randint(1, 4)
                 if random_action == 1:
                     self.current_action = 'sleep'
@@ -72,16 +72,12 @@ class Bunny:
                 elif random_action == 2:
                     self.current_action = 'eat'
                     self.img_sequence = self.eat
-                    self.action_duration = 5
-                elif random_action == 3:
-                    self.current_action = 'walk_right'
-                    self.img_sequence = self.walk_right
-                    self.action_duration = 5
-                elif random_action == 4:
-                    self.current_action = 'walk_left'
-                    self.img_sequence = self.walk_left
-                    self.action_duration = 5
-                time.sleep(3)
+                    self.action_duration = 8
+                elif random_action == 3 or random_action == 4:
+                    direction = 'walk_right' if random_action == 3 else 'walk_left'
+                    self.start_walking(direction)
+                    self.action_duration = 7
+                time.sleep(4)
 
         if not self.dragging:
             self.vel_y += self.gravity
@@ -98,24 +94,34 @@ class Bunny:
             self.img = self.img_sequence[frame_index]
 
             if self.current_action == 'walk_right':
-                self.x += 1
-                if self.x > self.window.winfo_screenwidth() - 110:
-                    self.current_action = 'idle'
-                    self.img = self.idle_blink[0]
+                if not self.moving_back and self.x > self.window.winfo_screenwidth() - 110:
+                    self.start_moving_back('left')
+                else:
+                    self.x += 1
             elif self.current_action == 'walk_left':
-                self.x -= 1
-                if self.x < 0:
-                    self.current_action = 'idle'
-                    self.img = self.idle[0]
+                if not self.moving_back and self.x < 0:
+                    self.start_moving_back('right')
+                else:
+                    self.x -= 1
 
         if current_time > self.last_action_time + self.action_duration:
             self.current_action = None
             self.img = self.idle[0]
+            self.moving_back = False
+
+    def start_walking(self, direction):
+        self.current_action = direction
+        self.img_sequence = self.walk_right if direction == 'walk_right' else self.walk_left
+        self.action_duration = random.randint(5, 7)
+
+    def start_moving_back(self, direction):
+        self.moving_back = True
+        self.start_walking('walk_right' if direction == 'right' else 'walk_left')
+        self.action_duration = 2
 
     def update_window(self):
         self.window.geometry('110x110+{x}+{y}'.format(x=str(int(self.x)), y=str(int(self.y))))
         self.label.configure(image=self.img)
-        self.label.pack()
 
     def start_drag(self, event):
         self.dragging = True
